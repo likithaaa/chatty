5babdb1521e34d9fccaf5651ac642403307c2867
likithaaa <likithad1@gmail.com>
2020-10-02 18:59:22 -0400

    Add main page

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAut-Wc3fXXvUyfs_2FZObXQ-S-5XyFVI4',
+  authDomain: 'chatty-1f82c.firebaseapp.com',
+  databaseURL: 'https://chatty-1f82c.firebaseio.com',
+  projectId: 'chatty-1f82c',
+  storageBucket: 'chatty-1f82c.appspot.com',
+  messagingSenderId: '796900732858',
+  appId: '1:796900732858:web:6857246fc4a7b399b6b783',
+  measurementId: 'G-GRXHQXS1L7',
+};
