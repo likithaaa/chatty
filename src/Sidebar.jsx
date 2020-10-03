2c610a470e7695bb60e922bef781468a61852926
likithaaa <likithad1@gmail.com>
2020-10-03 09:57:18 -0400

    Add icons for sidebar

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import './Sidebar.css';
+import { Avatar } from '@material-ui/core';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <div className="sidebarHeader"></div>
+      <div className="sidebarHeader">
+        <Avatar />
+        <div className="sidebarRight">
+          <DonutLargeIcon />
+          <ChatIcon />
+          <MoreVertIcon />
+        </div>
+      </div>
 
       <div className="sidebarSearch"></div>
+
+      <div className="sidebar_chats"></div>
     </div>
   );
 }
