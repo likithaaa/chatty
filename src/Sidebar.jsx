77860de01b3ede28c2605afc245971488a8cddbe
likithaaa <likithad1@gmail.com>
2020-10-03 10:10:27 -0400

    Make sidebar buttons clickable

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Sidebar.css';
-import { Avatar } from '@material-ui/core';
+import { Avatar, IconButton } from '@material-ui/core';
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
@@ -11,9 +11,15 @@ function Sidebar() {
       <div className="sidebarHeader">
         <Avatar />
         <div className="sidebarRight">
-          <DonutLargeIcon />
-          <ChatIcon />
-          <MoreVertIcon />
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
         </div>
       </div>
 
