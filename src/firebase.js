75b2b574f6ad35035d04962757c1a2ddd8bf39bd
likithaaa <likithad1@gmail.com>
2020-10-05 18:30:57 -0400

    Add initialization for firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
   apiKey: 'AIzaSyAut-Wc3fXXvUyfs_2FZObXQ-S-5XyFVI4',
   authDomain: 'chatty-1f82c.firebaseapp.com',
@@ -8,3 +10,5 @@ const firebaseConfig = {
   appId: '1:796900732858:web:6857246fc4a7b399b6b783',
   measurementId: 'G-GRXHQXS1L7',
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
