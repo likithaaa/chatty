5be1e17cf32ac8e92e921002b5bb09699350102b
likithaaa <likithad1@gmail.com>
2020-10-05 14:49:23 -0400

    Add styling for chat header

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Chat.css';
+import { Avatar, IconButton } from '@material-ui/core';
+import { SearchOutlined } from '@material-ui/icons';
+import AttachFileIcon from '@material-ui/icons/AttachFile';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 function Chat() {
+  const [seed, setSeed] = useState('');
+
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 5000));
+  }, []);
+
   return (
     <div className="chat">
-      <div className="chatHeader"></div>
+      <div className="chatHeader">
+        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+
+        <div className="chatHeaderInfo">
+          <h4>Room name</h4>
+        </div>
+
+        <div className="chatHeaderRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFileIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
 
       <div className="chatBody"></div>
 
